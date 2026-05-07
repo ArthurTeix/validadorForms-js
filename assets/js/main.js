3c8d127bfa7f838a2e7a454ee7d06285b4231883
Arthur Teixeira <126914319+ArthurTeix@users.noreply.github.com>
2026-05-06 23:25:31 -0300

    feat: add empty field validation to form

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,37 @@
 class ValidateForms {
-    constructor(){
+    constructor() {
         this.form = document.querySelector('.form-main')
         this.eventos()
     }
 
-    eventos(){
+    eventos() {
         this.form.addEventListener('submit', e => {
-            this.handleSubmit(e) // vai capturar o evento de envio e usar na função
+            this.handleSubmit(e)
         })
     }
 
-    handleSubmit(e){
-        e.preventDefault() // trava envio, para primeiro validar todos os campos
+    handleSubmit(e) {
+        e.preventDefault()
         const inputValids = this.isValid()
     }
 
-    isValid(){
+    isValid() {
+        let valid = true
 
+        for (let input of this.form.querySelectorAll('.validate')) {
+            if (!input.value) {
+                this.createError(input, 'Esse campo não pode estar vazio')
+                valid = false
+            }
+        }
+    }
+
+    createError(input, msg) {
+        const div = document.createElement('div')
+        div.innerHTML = msg
+        div.classList.add("error-text")
+
+        input.insertAdjacentElement('afterend', div)
     }
 }
 
